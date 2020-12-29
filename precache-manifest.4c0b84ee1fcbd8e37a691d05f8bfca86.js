83b0b28cdd065f57fc66d072ff0435e9d0d96b00
Ravikant Khandelwal <ravikant.khandelwal@bts.com>
2020-12-29 16:24:24 +0530

    Updates

---
+++ precache-manifest.4c0b84ee1fcbd8e37a691d05f8bfca86.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "99bf10d4705cfe5890673b929f22b95e",
+    "url": "/react-grocery/index.html"
+  },
+  {
+    "revision": "a0fe2c13d5b7192e1cb0",
+    "url": "/react-grocery/static/css/main.a903c182.chunk.css"
+  },
+  {
+    "revision": "6f28140f410c583af73d",
+    "url": "/react-grocery/static/js/2.18980cd5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-grocery/static/js/2.18980cd5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a0fe2c13d5b7192e1cb0",
+    "url": "/react-grocery/static/js/main.3abe471f.chunk.js"
+  },
+  {
+    "revision": "f7e3bfd61d209a8dd32e",
+    "url": "/react-grocery/static/js/runtime-main.3c3e46c3.js"
+  }
+]);
